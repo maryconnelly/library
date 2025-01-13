4f227459dc7cb5a79afe981ecbf7b23051ba0973
Mary Connelly <maryconnelly14@gmail.com>
2025-01-12 20:52:45 -0600

    Function to add new book object to array works

--- script.js
+++ script.js
@@ -1,53 +1,18 @@
 
 const myLibrary = [];
 
-//this creates new books
-function Book (title, author, pages) {
-	this.title = title;
+function Book (title, author, pages, read) {
+	this.title = title
 	this.author = author;
 	this.pages = pages;
+	this.read = read;
 }
 
-
-// this pushes new books to my library array
 function addBookToLibrary() {
-	
 
-
-
-}	
-
-
-/*
-function displayBooks() {
-
-	myLibrary.forEach(book => {
-
-		//make a new library book card
-		const bookCard = document.createElement('div');
-		bookCard.class = "book-card";
-		const libraryCardContainer = document.querySelector('#library-card-container');
-		libraryCardContainer.appendChild(bookCard);		
-
-		bookCard.appendChild(book.info);
-
-	})
-
-
+	myLibrary.push(new Book('title1', 'author1', 'pages1', 'read1')); 
 }
 
-function addBookButton() {
+addBookToLibrary();
 
-	const addButton = document.querySelector("#add");
-	addButton.addEventListener('click', function () {
-			
-		const formContainer = document.querySelector("#form-container");
-			formContainer.style.display = "grid";
-
-	
-			
-
-	})
-
-}
-*/
+console.log(myLibrary);
