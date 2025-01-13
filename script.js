7f12257ffc0745342dbfd594289516277c603465
Mary Connelly <maryconnelly14@gmail.com>
2025-01-12 21:58:42 -0600

    Form is displaying, need styles

--- script.js
+++ script.js
@@ -18,9 +18,17 @@ addBookToLibrary();
 
 console.log(myLibrary);
 
+//display book on card
 function displayBook() {
 
 	myLibrary.forEach(Book => {
+
+		const cardContainer = document.querySelector('#card-container');
+			cardContainer.style.display = "grid";
+			cardContainer.style.gridArea = "2 / 2 / 3 / 3";
+			cardContainer.style.border = "solid black 1px";
+			cardContainer.style.height = "600px";
+		document.body.appendChild(cardContainer);
 
 		const newCard = document.createElement('div');
 			newCard.textContent = `${Book.info}`;
@@ -29,10 +37,39 @@ function displayBook() {
 			newCard.style.width = "150px";
 			newCard.style.fontFamily = "'Courier New', Courier, monospace";
 			newCard.style.padding = "20px";
-			document.body.appendChild(newCard);
+			cardContainer.appendChild(newCard);
 
 	})
 }
 
 displayBook();
+
+//add book button 
+
+function addBookButton() {
+
+	const addBookButton = document.querySelector("#add");
+	document.body.appendChild(addBookButton);
+	addBookButton.addEventListener('click', () => {
+		
+		const sidebar = document.createElement('div');
+		document.body.appendChild(sidebar);
+		sidebar.id = "sidebar";
+		sidebar.style.display = "grid";
+		sidebar.style.gridArea = "2 / 1 / 3 / 2";
+		
+
+		const form = document.querySelector("form");
+		form.style.display = "grid";
+		form.style.border = "solid black 1px";
+		form.style.padding = "50px";
+		sidebar.appendChild(form);
+		
+
+	})
+
+}
+
+addBookButton();
+
 
