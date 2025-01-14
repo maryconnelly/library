aadd02e8923351fd5cbcfa5a8057c3a18fc413c4
Mary Connelly <maryconnelly14@gmail.com>
2025-01-14 17:32:56 -0600

    Adjust sidebar grid layout

--- script.js
+++ script.js
@@ -120,7 +120,7 @@ function addBookButton() {
 		const form = document.querySelector("form");
 		sidebar.appendChild(form);
 		form.style.display = "grid";
-		form.style.gridTemplate = "1fr 1fr .5fr/ 1fr";
+		form.style.gridTemplate = "1fr .5fr .5fr/ 1fr";
 
 
 	})
