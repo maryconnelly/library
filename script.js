a37f2a3f87254394c483d153e116f6b70e8e3420
Mary Connelly <maryconnelly14@gmail.com>
2025-01-22 09:29:53 -0600

    remove button removes array object and elements

--- script.js
+++ script.js
@@ -1,64 +1,55 @@
-
+const header = document.querySelector("#header");
+const openFormButton = document.querySelector("#open-form-button");
+const sidebar = document.querySelector("#sidebar");
+const form = document.querySelector("#form");
+const cardContainer = document.querySelector("#card-container");
 const myLibrary = [];
 
-function Book (title, author, pages, read) {
+function Book(title, author, pages, read) {
 	this.title = title
 	this.author = author;
 	this.pages = pages;
 	this.read = read;
-	this.id = crypto.randomUUID();
-	}
+}
 
 function addBookToLibrary() {
-
+	
 	document.querySelector('form').addEventListener('submit', (e) => {
 		e.preventDefault();
-
-		let titleInput = document.getElementById('titleInput').value;
-		let authorInput = document.getElementById('authorInput').value;
-		let pagesInput = document.getElementById('pagesInput').value;
-		let readInput = document.getElementById('readInput').value;
-
-	const newBook = new Book(titleInput, authorInput, pagesInput, readInput) 
-	myLibrary.push(newBook);
-	
-		const sidebar = document.querySelector("#sidebar");
-		sidebar.style.display = "none";
-
-		const openFormButton = document.querySelector("#open-form-button");
-		openFormButton.style.display = "block";
-
-		const cardContainer = document.querySelector("#card-container");
+		
+		let title = document.getElementById('title').value;
+		let author = document.getElementById('author').value;
+		let pages = document.getElementById('pages').value;
+		let status = document.getElementById('status').value;
+		
+		myLibrary.push(new Book(title, author, pages, status));
+		
 		cardContainer.style.gridArea = "2 / 1 / 3 / 3";
-
-	displayBook();
-
-	function resetForm() {
-		document.getElementById('form').reset();
-		}
-	resetForm();
-
+		
+		displayBook();
+		resetForm();
 	})
-	
 };
-
 addBookToLibrary();
 console.log(myLibrary);
 
+function resetForm() {
+	document.getElementById('form').reset();
+	openFormButton.style.display = "block";
+}
+
 function displayBook() {
+	
+	cardContainer.innerHTML = "";
 
-
-
-
-		const cardContainer = document.querySelector('#card-container');
-		document.body.appendChild(cardContainer);
+	for (let i=0;i<myLibrary.length; i++) {
+		let book = myLibrary[i];
 
 		const newCard = document.createElement('div');
 		cardContainer.appendChild(newCard);
 			newCard.class = "new-card";
-			newCard.id = `${Book.id}`;
 			newCard.style.display = "grid";
-			newCard.style.gridTemplate = "Repeat(4, 1fr) / 1fr";
+			newCard.style.gridTemplate = "1fr 1fr / 1fr";
 			newCard.style.backgroundColor = "#F5EFED";
 			newCard.style.height = "150px";
 			newCard.style.width = "200px";
@@ -66,80 +57,55 @@ function displayBook() {
 			newCard.style.fontSize = "18px";
 			newCard.style.fontWeight = "400";
 			newCard.style.padding = "15px";
+			newCard.innerHTML = `${book.title} by ${book.author} with ${book.pages} pages`;
+
 		
-		const titleLine = document.createElement('div');
-		newCard.appendChild(titleLine);
-		titleLine.textContent = `${Book.title}`;
-		titleLine.style.gridArea = "1 / 1 / 2 / 2";
-		titleLine.style.alignContent = "center";
+		const statusCheckbox = document.createElement('input');
+		statusCheckbox.type = "checkbox";
+		statusCheckbox.class = "status-checkbox";
+			statusCheckbox.style.justifySelf = "start";
+			statusCheckbox.style.alignSelf = "end";
+			statusCheckbox.style.gridArea = "3 / 1 / 4 / 2";
+		newCard.appendChild(statusCheckbox);
 
-		const authorLine = document.createElement('div');
-		newCard.appendChild(authorLine);
-		authorLine.textContent = `${Book.author}`;
-		authorLine.style.gridArea = "2 / 1 / 3 / 2";
-		authorLine.style.alignContent = "center";
-		
-
-		const pagesLine = document.createElement('div');
-		newCard.appendChild(pagesLine);
-		pagesLine.textContent = `${Book.pages}`;
-		pagesLine.style.gridArea = "3 / 1 / 4 / 2";
-		pagesLine.style.alignContent = "center";
-
-		const readLine = document.createElement('div');
-		newCard.appendChild(readLine);
-		readLine.style.gridArea = "4 / 1 / 5 / 2"
-		
-		const readCheckbox = document.createElement('input');
-		readCheckbox.type = "checkbox";
-		readCheckbox.id = "read-checkbox";
-		readLine.appendChild(readCheckbox);
-
-		const readLabel= document.createElement('label');
-		readLabel.for = "read-checkbox";
-		readLabel.textContent = "Read";
-		readLine.appendChild(readLabel);
-	
-
-		// const readToggle = document.createElement('button');
-		// newCard.appendChild(readToggle);
-		// readToggle.style.type = 'checkbox';
+		const statusLabel = document.createElement("p");
+		statusLabel.textContent = "Read";
+			statusLabel.style.alignSelf = "end";
+			statusLabel.style.gridArea = "3 / 1 / 4 / 2";
+			statusLabel.style.paddingLeft = "25px";
+		newCard.appendChild(statusLabel);
 
 		const removeButton = document.createElement('button');
-		newCard.appendChild(removeButton);
 		removeButton.type = "button";
-		removeButton.id = "remove";
+		removeButton.class = "remove-button";
 		removeButton.textContent = "Remove";
-		removeButton.style.gridArea = "4 / 1 / 5 / 2";
-
+			removeButton.style.gridArea = "3 / 1 / 4 / 2";
+			removeButton.style.placeSelf = "end";
+			removeButton.style.backgroundColor = "#BDBF09";
+			removeButton.style.fontSize = "16px";
+			removeButton.style.width = "85px";
+			removeButton.style.height = "35px";
+		newCard.appendChild(removeButton);
 		removeButton.addEventListener('click', () => {
-			if (Book.id === newCard.id ) {
-				const index = myLibrary.indexOf(Book.id);
-				delete myLibrary[index];
-				cardContainer.removeChild(newCard);
-		}})
+			myLibrary.splice([i], 1);
+			console.log(myLibrary);
+			cardContainer.removeChild(newCard);
+		})
 	}};
 
 
+		// const readToggle = document.createElement('button');
+		// newCard.appendChild(readToggle);
+		// readToggle.style.type=
+
+// //add book button 
+
 function openForm() {
-	const header = document.querySelector("#header");
-	const openFormButton = document.querySelector("#open-form-button");
-	header.appendChild(openFormButton);
 	openFormButton.addEventListener('click', () => {
 
 		openFormButton.style.display = "none";
-		
-		const cardContainer = document.querySelector('#card-container');
+		sidebar.style.display = "grid";
 		cardContainer.style.gridArea = "2/2/3/3";
-		
-		const sidebar = document.createElement('div');
-		document.body.appendChild(sidebar);
-		sidebar.id = "sidebar";
-		
-		const form = document.querySelector("form");
-		sidebar.appendChild(form);
-		form.style.display = "grid";
-		form.style.gridTemplate = " 2fr repeat(2, 1fr) / 1fr";
 	})
 }
 openForm();
